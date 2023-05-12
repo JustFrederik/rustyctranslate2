19a3ef5ce3a00316730dc42014d4bab740b05f41
frederik <frederikschwarz@icoud.com>
2023-05-13 01:53:41 +0200

    add options

--- include/translator.h
+++ include/translator.h
@@ -6,13 +6,24 @@
 
 #include "rust/cxx.h"
 
+class CTranslateOptions {
+public: CTranslateOptions(ctranslate2::TranslationOptions d) : data(d) {}
+  ctranslate2::TranslationOptions get() const {
+    return data;
+  }
+private: ctranslate2::TranslationOptions data;
+};
+
+// convert vec<vec<string>> from rust to c++ and vice versa
 class MyDataClass {
   public: MyDataClass(std::vector < std::vector < std::string >> data = {}): m_data(data) {}
 
+  // gets amount of sub vectors
   size_t getLength() const {
     return m_data.size();
   }
 
+  // gets sub vector
   rust::Vec < rust::String > getData(const size_t index) const {
     if (index >= m_data.size()) {
       throw std::out_of_range("Index out of range");
@@ -23,17 +34,27 @@ class MyDataClass {
     }
     return sentence;
   }
+
+  // extract data from class in c++
   const std::vector < std::vector < std::string >> get_all() const {
     return m_data;
   }
 
-  // Function to push a std::vector<std::string> to the data
+  // Function to push a std::vector<std::string> from rust to the data
   void pushData(rust::Vec < rust::String > newData) {
     std::vector < std::string > item;
     for (const auto & str: newData) {
       item.push_back(std::string(str));
     }
     m_data.push_back(item);
+  }
+
+  // memory management
+  ~MyDataClass() {
+    for (auto& item : m_data) {
+      item.clear();
+    }
+    m_data.clear();
   }
 
   private: std::vector < std::vector < std::string >> m_data;
@@ -48,19 +69,20 @@ class MyTranslator {
     }, {}) {}
 
   std::unique_ptr < MyDataClass > translate_batch(const MyDataClass & data,
+    const CTranslateOptions & options,
     const size_t max_batch_size = 0,
-      const bool batch_type_example = true) {
+    const bool batch_type_example = true ) {
     const std::vector < std::vector < std::string >> batch(data.get_all());
-    const auto options = ctranslate2::TranslationOptions();
-    auto translation = m_translator.translate_batch(batch, options, max_batch_size,
+    auto translation = m_translator.translate_batch(batch, options.get(), max_batch_size,
       batch_type_example ? ctranslate2::BatchType::Examples :
       ctranslate2::BatchType::Tokens);
     return std::make_unique < MyDataClass > (extract(translation));
   }
 
   std::unique_ptr < MyDataClass > translate_batch_target(const MyDataClass & data, rust::Vec < rust::String > target,
+    const CTranslateOptions & options,
     const size_t max_batch_size = 0,
-      const bool batch_type_example = true) {
+    const bool batch_type_example = true) {
     std::vector < std::vector < std::string >> target_v;
     for (const auto & t: target) {
       target_v.push_back({
@@ -68,8 +90,7 @@ class MyTranslator {
       });
     }
     const std::vector < std::vector < std::string >> batch(data.get_all());
-    const auto options = ctranslate2::TranslationOptions();
-    auto translation = m_translator.translate_batch(batch, target_v, options, max_batch_size,
+    auto translation = m_translator.translate_batch(batch, target_v, options.get(), max_batch_size,
       batch_type_example ? ctranslate2::BatchType::Examples :
       ctranslate2::BatchType::Tokens);
     return std::make_unique < MyDataClass > (extract(translation));
@@ -97,4 +118,40 @@ std::unique_ptr < MyTranslator > new_translator(const std::string & model,
 
 std::unique_ptr < MyDataClass > new_data() {
   return std::make_unique < MyDataClass > ();
+}
+
+std::unique_ptr<CTranslateOptions> get_options(
+    size_t beam_size = 2, float patience = 1, float length_penalty = 1,
+    float coverage_penalty = 0, float repetition_penalty = 1,
+    size_t no_repeat_ngram_size = 0, bool disable_unk = false,
+    float prefix_bias_beta = 0, bool return_end_token = false,
+    bool use_vmap = false, size_t num_hypotheses = 1,
+    bool return_scores = false, bool return_attention = false,
+    bool return_alternatives = false, float min_alternative_expansion_prob = 0,
+    bool replace_unknowns = false, size_t max_input_length = 1024,
+    size_t max_decoding_length = 256, size_t min_decoding_length = 1,
+    size_t sampling_topk = 1, float sampling_temperature = 1) {
+  auto v = ctranslate2::TranslationOptions();
+  v.beam_size = beam_size;
+  v.patience = patience;
+  v.length_penalty = length_penalty;
+  v.coverage_penalty = coverage_penalty;
+  v.repetition_penalty = repetition_penalty;
+  v.no_repeat_ngram_size = no_repeat_ngram_size;
+  v.disable_unk = disable_unk;
+  v.prefix_bias_beta = prefix_bias_beta;
+  v.return_end_token = return_end_token;
+  v.max_input_length = max_input_length;
+  v.max_decoding_length = max_decoding_length;
+  v.min_decoding_length = min_decoding_length;
+  v.sampling_topk = sampling_topk;
+  v.sampling_temperature = sampling_temperature;
+  v.use_vmap = use_vmap;
+  v.num_hypotheses = num_hypotheses;
+  v.return_scores = return_scores;
+  v.return_attention = return_attention;
+  v.return_alternatives = return_alternatives;
+  v.min_alternative_expansion_prob = min_alternative_expansion_prob;
+  v.replace_unknowns = replace_unknowns;
+  return std::make_unique<CTranslateOptions>(v);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 
-use cxx::{let_cxx_string, UniquePtr};
 use std::path::PathBuf;
+
+use cxx::{let_cxx_string, UniquePtr};
+use ffi::{CTranslateOptions, MyDataClass};
 
 use crate::ffi::MyTranslator;
 
@@ -18,12 +20,106 @@ impl BatchType {
     }
 }
 
+pub struct TranslationOptions {
+    /// Beam size to use for beam search (set 1 to run greedy search).
+    pub beam_size: usize,
+    /// Beam search patience factor, as described in https:///arxiv.org/abs/2204.05424.
+    /// The decoding will continue until beam_size*patience hypotheses are finished.
+    pub patience: f32,
+    /// Exponential penalty applied to the length during beam search.
+    /// The scores are normalized with:
+    ///   hypothesis_score /= (hypothesis_length ** length_penalty)
+    pub length_penalty: f32,
+    /// Coverage penalty weight applied during beam search.
+    pub coverage_penalty: f32,
+    /// Penalty applied to the score of previously generated tokens, as described in
+    /// https:///arxiv.org/abs/1909.05858 (set > 1 to penalize).
+    pub repetition_penalty: f32,
+    /// Prevent repetitions of ngrams with this size (set 0 to disable).
+    pub no_repeat_ngram_size: usize,
+    /// Disable the generation of the unknown token.
+    pub disable_unk: bool,
+    /// Biases decoding towards a given prefix, see https:///arxiv.org/abs/1912.03393 --section 4.2
+    /// Only activates biased-decoding when beta is in range (0, 1) and SearchStrategy is set to BeamSearch.
+    /// The closer beta is to 1, the stronger the bias is towards the given prefix.
+    ///
+    /// If beta <= 0 and a non-empty prefix is given, then the prefix will be used as a
+    /// hard-prefix rather than a soft, biased-prefix.
+    pub prefix_bias_beta: f32,
+
+    /// Include the end token in the result.
+    pub return_end_token: bool,
+
+    /// Truncate the inputs after this many tokens (set 0 to disable truncation).
+    pub max_input_length: usize,
+
+    /// Decoding length constraints.
+    pub max_decoding_length: usize,
+    pub min_decoding_length: usize,
+
+    /// Randomly sample from the top K candidates (set 0 to sample from the full output distribution).
+    pub sampling_topk: usize,
+    /// High temperature increase randomness.
+    pub sampling_temperature: f32,
+
+    /// Allow using the vocabulary map included in the model directory, if it exists.
+    pub use_vmap: bool,
+
+    /// Number of hypotheses to store in the TranslationResult class.
+    pub num_hypotheses: usize,
+
+    /// Store scores in the TranslationResult class.
+    pub return_scores: bool,
+    /// Store attention vectors in the TranslationResult class.
+    pub return_attention: bool,
+
+    /// Return alternatives at the first unconstrained decoding position. This is typically
+    /// used with a target prefix to provide alternatives at a specifc location in the
+    /// translation.
+    pub return_alternatives: bool,
+    /// Minimum probability to expand an alternative.
+    pub min_alternative_expansion_prob: f32,
+
+    /// Replace unknown target tokens by the original source token with the highest attention.
+    pub replace_unknowns: bool,
+}
+
+impl Default for TranslationOptions {
+    fn default() -> Self {
+        Self {
+            beam_size: 2,
+            patience: 1.0,
+            length_penalty: 1.0,
+            coverage_penalty: 0.0,
+            repetition_penalty: 1.0,
+            no_repeat_ngram_size: 0,
+            disable_unk: false,
+            prefix_bias_beta: 0.0,
+            return_end_token: false,
+            max_input_length: 1024,
+            max_decoding_length: 256,
+            min_decoding_length: 1,
+            sampling_topk: 1,
+            sampling_temperature: 1.0,
+            use_vmap: false,
+            num_hypotheses: 1,
+            return_scores: false,
+            return_attention: false,
+            return_alternatives: false,
+            min_alternative_expansion_prob: 0.0,
+            replace_unknowns: false,
+        }
+    }
+}
+
 #[cxx::bridge()]
 mod ffi {
+
     unsafe extern "C++" {
         include!("rustyctranslate2/include/translator.h");
         type MyTranslator;
         type MyDataClass;
+        type CTranslateOptions;
         fn new_translator(
             model: &CxxString,
             use_gpu: bool,
@@ -32,6 +128,7 @@ mod ffi {
         fn translate_batch(
             self: Pin<&mut MyTranslator>,
             data: &MyDataClass,
+            options: &CTranslateOptions,
             max_batch_size: usize,
             batch_type_example: bool,
         ) -> Result<UniquePtr<MyDataClass>>;
@@ -39,6 +136,7 @@ mod ffi {
             self: Pin<&mut MyTranslator>,
             data: &MyDataClass,
             target: Vec<String>,
+            options: &CTranslateOptions,
             max_batch_size: usize,
             batch_type_example: bool,
         ) -> Result<UniquePtr<MyDataClass>>;
@@ -46,6 +144,17 @@ mod ffi {
         fn getLength(self: &MyDataClass) -> usize;
         fn pushData(self: Pin<&mut MyDataClass>, item: Vec<String>);
         fn getData(self: &MyDataClass, data: usize) -> Result<Vec<String>>;
+        #[allow(clippy::too_many_arguments)]
+        fn get_options(beam_size: usize, patience: f32,  length_penalty : f32,
+                       coverage_penalty : f32,  repetition_penalty : f32,
+                       no_repeat_ngram_size : usize,  disable_unk : bool,
+                       prefix_bias_beta : f32,  return_end_token : bool,
+                       use_vmap : bool,  num_hypotheses : usize,
+                       return_scores : bool,  return_attention : bool,
+                       return_alternatives : bool,  min_alternative_expansion_prob : f32,
+                       replace_unknowns : bool,  max_input_length : usize,
+                       max_decoding_length : usize,  min_decoding_length : usize,
+                       sampling_topk : usize,  sampling_temperature : f32) -> UniquePtr<CTranslateOptions>;
     }
 }
 
@@ -65,20 +174,57 @@ impl CTranslator {
         &mut self,
         input: Vec<Vec<String>>,
         max_batch_size: Option<usize>,
+        options: Option<TranslationOptions>,
         batch_type: BatchType,
     ) -> Result<Vec<Vec<String>>, String> {
+        let data = Self::generate_input(input)?;
+        let options = self.get_options(options);
+        let v = self
+            .model
+            .as_mut()
+            .ok_or_else(|| "mut model is none".to_string())?
+            .translate_batch(&data, &options, max_batch_size.unwrap_or(0), batch_type.to_bool())
+            .map_err(|e| e.to_string())?;
+        Self::extract_output(v)
+    }
+
+    pub fn translate_batch_target(
+        &mut self,
+        input: Vec<Vec<String>>,
+        max_batch_size: Option<usize>,
+        batch_type: BatchType,
+        options: Option<TranslationOptions>,
+        target: Vec<String>,
+    ) -> Result<Vec<Vec<String>>, String> {
+        let data = Self::generate_input(input)?;
+        let options = self.get_options(options);
+        let v = self
+            .model
+            .as_mut()
+            .ok_or_else(|| "mut model is none".to_string())?
+            .translate_batch_target(
+                &data,
+                target,
+                &options,
+                max_batch_size.unwrap_or(0),
+                batch_type.to_bool(),
+            )
+            .map_err(|e| e.to_string())?;
+        Self::extract_output(v)
+        
+    }
+
+    fn generate_input(input: Vec<Vec<String>>) -> Result<UniquePtr<MyDataClass>, String> {
         let mut data = ffi::new_data();
         for item in input {
             data.as_mut()
                 .ok_or_else(|| "mut mydataclass is none".to_string())?
                 .pushData(item);
         }
-        let v = self
-            .model
-            .as_mut()
-            .ok_or_else(|| "mut model is none".to_string())?
-            .translate_batch(&data, max_batch_size.unwrap_or(0), batch_type.to_bool())
-            .map_err(|e| e.to_string())?;
+        Ok(data)
+    }
+
+    fn extract_output(v: UniquePtr<MyDataClass>) -> Result<Vec<Vec<String>>, String> {
         let mut res = vec![];
         let length = v.getLength();
         for index in 0..length {
@@ -87,36 +233,9 @@ impl CTranslator {
         Ok(res)
     }
 
-    pub fn translate_batch_target(
-        &mut self,
-        input: Vec<Vec<String>>,
-        max_batch_size: Option<usize>,
-        batch_type: BatchType,
-        target: Vec<String>,
-    ) -> Result<Vec<Vec<String>>, String> {
-        let mut data = ffi::new_data();
-        for item in input {
-            data.as_mut()
-                .ok_or_else(|| "mut mydataclass is none".to_string())?
-                .pushData(item);
-        }
-        let v = self
-            .model
-            .as_mut()
-            .ok_or_else(|| "mut model is none".to_string())?
-            .translate_batch_target(
-                &data,
-                target,
-                max_batch_size.unwrap_or(0),
-                batch_type.to_bool(),
-            )
-            .map_err(|e| e.to_string())?;
-        let mut res = vec![];
-        let length = v.getLength();
-        for index in 0..length {
-            res.push(v.getData(index).map_err(|e| e.to_string())?);
-        }
-        Ok(res)
+    fn get_options(&self, options: Option<TranslationOptions>) -> UniquePtr<CTranslateOptions>{
+        let o = options.unwrap_or_default();
+        ffi::get_options(o.beam_size, o.patience, o.length_penalty, o.coverage_penalty, o.repetition_penalty, o.no_repeat_ngram_size, o.disable_unk, o.prefix_bias_beta, o.return_end_token, o.use_vmap, o.num_hypotheses, o.return_scores, o.return_attention, o.return_alternatives, o.min_alternative_expansion_prob, o.replace_unknowns, o.max_input_length, o.max_decoding_length, o.min_decoding_length, o.sampling_topk, o.sampling_temperature)
     }
 }
 
@@ -128,7 +247,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let model = CTranslator::new(PathBuf::from_str("jparacrawl/base-en-ja").unwrap(), false);
+        let model = CTranslator::new(PathBuf::from_str("jparacrawl/base-en-ja").unwrap(), false, true);
         assert!(model.is_ok());
         let tokens = ["▁H", "ell", "o", "▁world", "!"]
             .into_iter()
@@ -136,7 +255,7 @@ mod tests {
             .collect();
         let v = model
             .unwrap()
-            .translate_batch(vec![tokens], None, BatchType::Example);
+            .translate_batch(vec![tokens], None, None, BatchType::Example);
         assert!(v.is_ok());
         println!("{:?}", v);
     }
